2c22770cedd898d30ead370a0dfb418fb0b57453
PnCeDk <minhdanglee2006@gmail.com>
2026-01-13 00:12:40 +0700

    feat: added Contact and Footer components

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,8 +1,26 @@
 import { assets } from "@/assets/assets";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "0a60a870-daa4-4dc1-abac-5e2ea45d27ce");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
   return (
     <div
       id="contact"
@@ -16,19 +34,21 @@ const Contact = () => {
         feedback, please use the form below.
       </p>
 
-      <form className="max-w-2xl mx-auto">
+      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
         <div className="grid grid-cols-auto gap-6 mt-10 mb-8">
           <input
             type="text"
             placeholder="Enter you name"
             required
             className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+            name="name"
           />
           <input
             type="email"
             placeholder="Enter you email"
             required
             className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+            name="email"
           />
         </div>
         <textarea
@@ -36,6 +56,7 @@ const Contact = () => {
           placeholder="Enter your message"
           required
           className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+          name="message"
         ></textarea>
 
         <button
@@ -46,7 +67,7 @@ const Contact = () => {
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </button>
 
-        <p className="mt-4"></p>
+        <p className="mt-4">{result}</p>
       </form>
     </div>
   );
